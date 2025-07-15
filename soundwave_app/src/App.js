bfef06c5e2e35a472a354ba16c8a93f726916132
nalcoop <nic4340@rit.edu>
2025-07-14 21:38:27 -0400

    app function and ui created and tested

--- soundwave_app/src/App.js
+++ soundwave_app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// react file 
+// parse the access token
+// author nalin cooper
+
+
+import React, {useEffect, useState} from "react";
+import { authEndpoint } from "./authentication";
+
+
+// function will parse through the users spotify information
+function App(){
+    const [token,setToken] = useState("");
+
+    useEffect(() =>{
+        const hash= window.location.hash;
+        let _token = window.localStorage.getItem("spotify_token");
+
+        if(!_token && hash){
+            _token = hash
+            .substring(1)
+            .split("&")
+            .find((elem)=> elem.startsWith("access_token"))
+            .split("=")[1];
+
+            window.location.hash="";
+            window.localStorage.setItem("spotify_token", _token);
+        }
+
+        setToken(_token);
+    }, []);
+
+    return (
+        <div>
+            {!token ? (
+                <a href={authEndpoint}>
+                    <h1>Upload a Song or Login with Spoify</h1>
+                    <canvas id='soundwaveCanvas' width={800} height={400}></canvas>
+                    <br></br>
+                    <button>Login with Spotify</button>
+                    <input type='file' id='audioFile' accept='audio/*'></input>
+                    <button onClick={"play()"}>Play</button>
+                    <button onClick={"pause()"}>Pause</button>
+                </a>
+            ) : (
+                <p>Logged in! Token: {token.substring(0,10)}... </p>
+            )}
+        </div>
+    );
 }
+
 
 export default App;
